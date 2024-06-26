e9dcf25d5fede8f4cc5171a069174e8b90abfabd
Username10924 <129519733+Username10924@users.noreply.github.com>
2024-06-27 00:27:44 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Sign-up-form
+# Sign-up-form  
+A sign up form template with client-side validation  
+Warning: a server-side validation is still required, malicious clients can easily bypass client-side validations.
---
+++ match-password.js
@@ -0,0 +1,16 @@
+let password = document.querySelector("#password");
+let passwordConfirm = document.querySelector("#passwordConfirm");
+let submitBtn = document.querySelector(".submit");
+let passwordError = document.createElement("p");
+passwordError.textContent = "Passwords do not match!"
+passwordError.style.color = "var(--secondary)";
+passwordError.style.fontSize = "small";
+let passwordItem = document.querySelector(".password");
+submitBtn.addEventListener("click", (e) => {
+    if(password.value != passwordConfirm.value) {
+        passwordItem.appendChild(passwordError);
+    }
+    else if(passwordItem.contains(passwordItem)) {
+        passwordItem.removeChild(passwordError);
+    }
+});
